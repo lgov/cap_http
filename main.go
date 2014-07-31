7b3d38f555a9db640133cc6eb5a8b5e543dbed24
Lieven Govaerts <lgo@mobsol.be>
2014-07-31 22:53:43 +0200

    Separate streams in 'in' and 'out'. Read http responses from the 'in' stream.

--- main.go
+++ main.go
@@ -17,6 +17,11 @@ import (
 var iface = flag.String("i", "lo0", "Interface to get packets from")
 var logAllPackets = flag.Bool("v", false, "Logs every packet in great detail")
 
+type bidiStream struct {
+	key     uint64
+	in, out *httpStream
+}
+
 // httpStream will handle the actual decoding of http requests and responses.
 type httpStream struct {
 	netFlow, tcpFlow gopacket.Flow
@@ -24,9 +29,9 @@ type httpStream struct {
 }
 
 /* This reads both HTTP requests and HTTP responses in two separate streams */
-func (h *httpStream) run() {
-	log.Printf("run")
-	log.Println("reading stream", h.netFlow, h.tcpFlow)
+func (h *httpStream) runOut() {
+	log.Printf("runOut")
+
 	buf := bufio.NewReader(&h.readStream)
 	for {
 		req, err := http.ReadRequest(buf)
@@ -38,21 +43,70 @@ func (h *httpStream) run() {
 		} else {
 			bodyBytes := tcpreader.DiscardBytesToEOF(req.Body)
 			req.Body.Close()
-			log.Println("Received request from stream", h.netFlow, h.tcpFlow, ":", req, "with", bodyBytes, "bytes in request body")
+			log.Println("Received request from stream", h.netFlow, h.tcpFlow,
+				":", req, "with", bodyBytes, "bytes in request body")
 		}
 	}
 }
 
+func (h *httpStream) runIn() {
+	log.Printf("runIn")
+
+	buf := bufio.NewReader(&h.readStream)
+	for {
+
+		/* Don't start reading a response if no data is available */
+		_, err := buf.Peek(1)
+		if err == io.EOF {
+			return
+		}
+		/* Data available, read response */
+		resp, err := http.ReadResponse(buf, nil) // TODO: request
+		if err == io.EOF {
+			// We must read until we see an EOF... very important!
+			log.Println("EOF while reading stream", h.netFlow, h.tcpFlow, ":", err)
+			return
+		} else if err != nil {
+			log.Println("Error reading stream", h.netFlow, h.tcpFlow, ":", err)
+		} else {
+			bodyBytes := tcpreader.DiscardBytesToEOF(resp.Body)
+			resp.Body.Close()
+			log.Println("Received response from stream", h.netFlow, h.tcpFlow,
+				":", resp, "with", bodyBytes, "bytes in response body")
+		}
+
+		/* Match the response with the next request */
+	}
+
+}
+
 /* httpStreamFactory implements tcpassembly.StreamFactory */
-type httpStreamFactory struct{}
+type httpStreamFactory struct {
+	bidiStreams map[uint64]*bidiStream
+}
 
 func (h *httpStreamFactory) New(netFlow, tcpFlow gopacket.Flow) tcpassembly.Stream {
+
+	/* Can we find out if this is the outgoing or incoming stream? */
+	/* First the outgoing stream, then the incoming stream */
+	key := netFlow.FastHash() ^ tcpFlow.FastHash()
+
 	hstream := &httpStream{
 		netFlow:    netFlow,
 		tcpFlow:    tcpFlow,
 		readStream: tcpreader.NewReaderStream(),
 	}
-	go hstream.run() // Important... we must guarantee that data from the reader stream is read.
+
+	bds := h.bidiStreams[key]
+	if bds == nil {
+		log.Println("new stream", key)
+		bds = &bidiStream{in: hstream, key: key}
+		h.bidiStreams[key] = bds
+		go hstream.runOut() // Important... we must guarantee that data from the reader stream is read.
+	} else {
+		bds.out = hstream
+		go hstream.runIn() // Important... we must guarantee that data from the reader stream is read.
+	}
 
 	// ReaderStream implements tcpassembly.Stream, so we can return a pointer to it.
 	return &hstream.readStream
@@ -70,7 +124,7 @@ func main() {
 	}
 
 	// Set up assembly
-	streamFactory := &httpStreamFactory{}
+	streamFactory := &httpStreamFactory{bidiStreams: make(map[uint64]*bidiStream)}
 	streamPool := tcpassembly.NewStreamPool(streamFactory)
 	assembler := tcpassembly.NewAssembler(streamPool)
 
